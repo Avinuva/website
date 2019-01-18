15ab6880f1cf7d54790163f7737baa7d41c4d1b4
Mohamed Meftah <mohamed-ted@live.com>
2019-01-18 14:26:04 +0100

    add publish task

--- build/tasks/publish.ts
+++ build/tasks/publish.ts
@@ -1,25 +1,38 @@
-import gulp from 'gulp'
+import { spawn } from 'child_process'
+import listr from 'listr'
+
 import { outputs } from '../gulp.config'
 
-import { spawn } from 'child_process'
-
-function git(dir: string, ...args) {
-  new Promise((resolve, reject) => {
-    console.log('start', args)
-
-    const git = spawn('git', args,{ cwd: dir })
-    git.stdout.on('data', d => console.log(d.toString()))
-    git.stderr.on('data', d => console.log(d.toString()))
-    git.on('close', code => {
-      resolve()
-    })
+function git(...args) {
+  return new Promise((resolve, reject) => {
+    const git = spawn('git', args, { cwd: outputs.bundle })
+    git.on('error', e => reject())
+    git.on('close', () => resolve())
   })
 }
 
-export default async function  publish() {
-
-    await git(outputs.bundle, 'init')
-    await git(outputs.bundle, 'add', '.')
-    await git(outputs.bundle, 'commit', '-m', 'update page')
-    return git(outputs.bundle, 'push', '--force', 'git@github.com:netulip/website-frontend.git', 'master:gh-pages')
+export default async function publish() {
+  const tasks = new listr([
+    {
+      title: 'Initializing repository',
+      task: () => git('init'),
+    },
+    {
+      title: 'Adding files',
+      task: () => git('add', '.'),
+    },
+    {
+      title: 'Commiting changes',
+      task: () => git('commit', '-m', 'update page'),
+    },
+    {
+      title: 'Deleleting old version',
+      task: () => git('push', '--progress', '--delete', 'git@github.com:netulip/website-frontend.git', 'gh-pages'),
+    },
+    {
+      title: 'Pushing new version',
+      task: () => git('push', '--progress' , '--force', 'git@github.com:netulip/website-frontend.git', 'master:gh-pages'),
+    },
+  ])
+  return tasks.run()
 }
